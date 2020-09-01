9cd323d071dee7b44f86a4c7b3db2ed82042c85a
AndrewDAntonio <github email address>
2020-08-31 20:56:41 -0400

    added form

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import {MdSend} from 'react-icons/md';
 
 export const ExpenseForm = () => {
 
     return (
-        <div>ExpenseForm</div>
+        <form>
+            <div className="form-center">
+                <div className='form-group'>
+                    <label htmlFor='charge'>Charge</label>
+                    <input 
+                        type="text" 
+                        className='form-control' 
+                        id='charge' 
+                        name='charge' 
+                        placeholder="ex: rent"
+                        />
+                </div>
+            </div>
+        </form>
     )
 }
 
